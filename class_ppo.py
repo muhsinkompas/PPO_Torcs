aa5e2a18cd9883bbf23ae1ac79d2f049633caebb
muhsinkompas <muhsinkompas@gmail.com>
2023-08-08 02:13:31 +0300

    scalar mult deleted.

--- class_ppo.py
+++ class_ppo.py
@@ -172,9 +172,9 @@ class PPO(object):
             sigma_st = tf.layers.dense(l3, 1, activation=tf.nn.sigmoid, trainable=trainable, kernel_initializer=rand_unif, bias_initializer=bias_const)
             sigma_acc = tf.layers.dense(l3, 1, activation=tf.nn.sigmoid, trainable=trainable, kernel_initializer=rand_unif, bias_initializer=bias_const)
             sigma_br = tf.layers.dense(l3, 1, activation=tf.nn.sigmoid, trainable=trainable, kernel_initializer=rand_unif, bias_initializer=bias_const)
-            sigma_st = tf.scalar_mul(0.2,sigma_st) # scalar mult            
-            sigma_acc = tf.scalar_mul(0.2,sigma_acc) # scalar mult 
-            sigma_br = tf.scalar_mul(0.05,sigma_br) # scalar mult 
+            #sigma_st = tf.scalar_mul(0.2,sigma_st) # scalar mult            
+            #sigma_acc = tf.scalar_mul(0.2,sigma_acc) # scalar mult 
+            #sigma_br = tf.scalar_mul(0.05,sigma_br) # scalar mult 
             sigma = tf.concat([sigma_st, sigma_acc, sigma_br], axis=1)          
             sigma = tf.clip_by_value(sigma,0.0+small,1.0-small)
 
