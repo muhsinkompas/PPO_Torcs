e74c1382c0874b7f3126894bec742e0ecbefc4a3
muhsinkompas <muhsinkompas@gmail.com>
2023-08-08 19:54:50 +0100

    8aug

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ C_UPDATE_STEPS = 10
 S_DIM, A_DIM = 29, 3
 METHOD = [
     dict(name='kl_pen', kl_target=0.01, lam=1.0),   # KL penalty; lam is actually beta from the PPO paper
-    dict(name='clip', epsilon=0.20),           # Clipped surrogate objective, find this is better
+    dict(name='clip', epsilon=0.15),           # Clipped surrogate objective, find this is better
 ][1]        # choose the method for optimization
 
 
